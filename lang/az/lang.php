948173ae4b52766d2013f67e36df3f65f90f325b
Aleksandr Selivanov <alexgearbox@yandex.ru>
2021-01-23 17:20:08 +0100

    translation update

---
+++ lang/az/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['yearbox_months_jan']    = 'Yan';
+$lang['yearbox_months_feb']    = 'Fev';
+$lang['yearbox_months_mar']    = 'Mar';
+$lang['yearbox_months_apr']    = 'Apr';
+$lang['yearbox_months_may']    = 'Bil';
+$lang['yearbox_months_jun']    = 'Iyu';
+$lang['yearbox_months_jul']    = 'Iyu';
+$lang['yearbox_months_aug']    = 'Avq';
+$lang['yearbox_months_sep']    = 'Sen';
+$lang['yearbox_months_oct']    = 'Okt';
+$lang['yearbox_months_nov']    = 'Noy';
+$lang['yearbox_months_dec']    = 'Dek';
+$lang['yearbox_days']          = 'SBÇÇCCŞ';
